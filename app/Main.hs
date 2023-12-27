1b27453a89fa2bb22b69a93e04c34364a4dad9d6
Syllina <syllinxia@gmail.com>
2023-12-27 10:53:03 +0800

    files created by summoner

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import Hsco (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/Hsco.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2023 Syllinxia
+SPDX-License-Identifier: MIT
+Maintainer: Syllinxia <syllinxia@gmail.com>
+
+The very least comprehensive personal haskell tool
+-}
+
+module Hsco
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
